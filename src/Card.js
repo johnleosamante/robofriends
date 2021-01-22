bc0dd4ce91997bb72178c59b1b342b85680df6b9
John Leo Samante <johnleomuitsamante@gmail.com>
2021-01-22 20:37:38 +0800

    Adding project files

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img
+        alt="robots"
+        src="https://robohash.org/leo?set=any?size=200x200?bgset=any"
+      />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>janedoe@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
